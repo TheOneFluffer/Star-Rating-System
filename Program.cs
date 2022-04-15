e983e701353db7db41008f3f18dda247ff2cd375
TheOneFluffer <singingfurbyx3@gmail.com>
2022-04-15 15:39:07 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Star_Rating_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Type your number and press enter
+            Console.Write("Enter a number between 1 to 5: ");
+
+            //Create a variable and get user input from the keyboard and store it in the variable
+            decimal StarRating = Convert.ToDecimal(Console.ReadLine());
+            decimal i = 1;
+
+            //Create a loop to print output
+            do
+            {
+                Console.Write("Full ");
+                i++;
+            }
+            while (i <= StarRating);
+            {
+                Console.Write("Half ");
+            }
+        }
+    }
+}
